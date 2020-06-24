60bc654b2f0ecc78724490b87e4e4123b78cd0b2
volatile-static <1043064987@qq.com>
2020-06-24 14:56:43 +0800

    多项式乘除

--- 实验一 多项式加减实现/多项式.py
+++ 实验一 多项式加减实现/多项式.py
@@ -7,7 +7,10 @@ class PolyItem:
         self.exponential = expon  # 指数
 
     def __str__(self):
-        return '%d %d' % (self.coefficient, self.exponential)
+        if self.coefficient == 0:
+            return '0'
+        else:
+            return '%dx^{%d}' % (self.coefficient, self.exponential)
     
     def __add__(self, value):
         return PolyItem(self.coefficient + value.coefficient, self.exponential)
@@ -25,6 +28,7 @@ class PolyItem:
         return self.exponential > value.exponential
 
     def __eq__(self, value):
+        '指数相等'
         return self.exponential == value.exponential
 
 
@@ -33,15 +37,22 @@ def Merge(a1, a2, add_sub):  # 归并有序表并去重
     p1 = p2 = 0
     while p1 < len(a1) and p2 < len(a2):
         if a1[p1] > a2[p2]:
-            tmp.items.append(a2[p2])
+            tmp.append(a2[p2])
             p2 += 1
-        elif a1[p1] == a1[p2]:
-            tmp.items.append(a1[p1] + a2[p2] if add_sub else a1[p1] - a2[p2])
+        elif a1[p1] == a2[p2]:
+            tmp.append(a1[p1] + a2[p2] if add_sub else a1[p1] - a2[p2])
             p1 += 1
             p2 += 1
         else:
-            tmp.items.append(a1[p1])
+            tmp.append(a1[p1])
             p1 += 1
+
+    while p1 < len(a1):
+        tmp.append(a1[p1])
+        p1 += 1
+    while p2 < len(a2):
+        tmp.append(a2[p2])
+        p2 += 1
     return tmp
 
 
@@ -61,8 +72,8 @@ class Polynomial:
     def __str__(self):
         ret = ''
         for it in self.items:
-            ret += '%dx^%d+' % (it.coefficient, it.exponential)
-        return ret[0 : len(ret) - 1]  # 去掉末尾的加号
+            ret += str(it) + '+'
+        return '$' + ret[0 : len(ret) - 1] + '$'  # 去掉末尾的加号
 
     def __add__(self, other):
         ret = Polynomial('')
@@ -73,3 +84,33 @@ class Polynomial:
         ret = Polynomial('')
         ret.items = Merge(self.items, other.items, False)
         return ret
+
+    def __mul__(self, other):
+        '卷积'
+        ret = Polynomial('')
+        for i in self.items:
+            for j in other.items:
+                ret.items.append(i * j)
+        ret.items.sort()
+        ret.Simplify()
+        return ret
+
+    def __truediv__(self, other):
+        '寻找同类项相除'
+        ret = Polynomial('')
+        for den in other.items:
+            if den in self.items:
+                ret.items.append(self.items[self.items.index(den)] / den)
+            else:
+                ret.items.append(PolyItem(1, 0) / den)
+        return ret
+
+    def Simplify(self):
+        '合并同类项'
+        i = 1
+        while i < len(self.items):
+            if self.items[i] == self.items[i - 1]:
+                self.items[i - 1] += self.items[i]
+                self.items.pop(i)
+            else:
+                i += 1
