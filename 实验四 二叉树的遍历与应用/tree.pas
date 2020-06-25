abab7028003ae519f1d411178222a25efcaf8f2c
volatile-static <1043064987@qq.com>
2020-06-25 09:54:44 +0800

    二叉树

--- 实验四 二叉树的遍历与应用/tree.pas
+++ 实验四 二叉树的遍历与应用/tree.pas
@@ -11,16 +11,85 @@ type
     root: ^TTreeNode;
   public
     constructor Create();
+    procedure First(n : TTreeNode);
+    procedure Middle(n : TTreeNode);
+    procedure Last(n : TTreeNode);
+    procedure BFS;
+    function Depth(n : TTreeNode) : Integer;
   end;
 
 implementation
-uses main;
+uses main, math, System.SysUtils;
+
+procedure BuildPre(n : TTreeNode);
+begin
+  read(n.id);
+  if n.id = #13 then  // 遇到回车结束
+    Exit;
+  new(n.left);
+  BuildPre(n.left^);
+  new(n.right);
+  BuildPre(n.right^);
+end;
 
 constructor TBinaryTree.Create();
 begin
-
   new(root);
-  dispose(root);
+  BuildPre(root^);
+  {dispose(root);}
+end;
+
+procedure TBinaryTree.First(n : TTreeNode);
+begin
+  write(n.id);
+  if n.left <> nil then
+    First(n.left^);
+  if n.right <> nil then
+    First(n.right^);
+end;
+
+procedure TBinaryTree.Middle(n : TTreeNode);
+begin
+  if n.left <> nil then
+    First(n.left^);
+  write(n.id);
+  if n.right <> nil then
+    First(n.right^);
+end;
+
+procedure TBinaryTree.Last(n : TTreeNode);
+begin
+  if n.left <> nil then
+    First(n.left^);
+  if n.right <> nil then
+    First(n.right^);
+  write(n.id);
+end;
+
+procedure TBinaryTree.BFS;
+var
+  queue : OrderQueue;
+  n : ^TTreeNode;
+begin
+  queue.push_back(root^);
+  while not queue.empty() do
+    begin
+        n := queue.front();
+        write(n.id);
+        queue.pop_front();
+
+        if n.left <> nil then
+            queue.push_back(n.left);
+        if n.right <> nil then
+            queue.push_back(n.right);
+    end
+end;
+
+function TBinaryTree.Depth(n : TTreeNode) : Integer;
+begin
+  if (n.left = nil) and (n.right = nil) then
+    Exit(1);
+  Result := 1 + max(Depth(n.left^), Depth(n.right^));
 end;
 
 end.
