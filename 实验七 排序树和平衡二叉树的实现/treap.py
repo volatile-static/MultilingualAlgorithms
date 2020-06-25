66f8a7f704d8a4c24175aef4f230e140b004b110
volatile-static <1043064987@qq.com>
2020-06-25 12:31:05 +0800

    平衡树

--- 实验七 排序树和平衡二叉树的实现/treap.py
+++ 实验七 排序树和平衡二叉树的实现/treap.py
@@ -3,11 +3,6 @@ from typing import Tuple
 
 
 class Node:
-    """
-    Treap's node
-    Treap is a binary tree by value and heap by priority
-    """
-
     def __init__(self, value: int = None):
         self.value = value
         self.prior = random()
@@ -24,69 +19,47 @@ class Node:
                 {f"{self.value}: {self.prior:.5}": (self.left, self.right)}, indent=1
             )
 
-    def __str__(self):
-        value = str(self.value) + " "
-        left = str(self.left or "")
-        right = str(self.right or "")
-        return value + left + right
-
 
 def split(root: Node, value: int) -> Tuple[Node, Node]:
     """
-    We split current tree into 2 trees with value:
-    Left tree contains all values less than split value.
-    Right tree contains all values greater or equal, than split value
+    用给定值划分左右子树
+    左边小右边大
     """
-    if root is None:  # None tree is split into 2 Nones
+    if root is None: 
         return (None, None)
     elif root.value is None:
         return (None, None)
     else:
         if value < root.value:
-            """
-            Right tree's root will be current node.
-            Now we split(with the same value) current node's left son
-            Left tree: left part of that split
-            Right tree's left son: right part of that split
-            """
             left, root.left = split(root.left, value)
             return (left, root)
         else:
-            """
-            Just symmetric to previous case
-            """
             root.right, right = split(root.right, value)
             return (root, right)
 
 
 def merge(left: Node, right: Node) -> Node:
     """
-    We merge 2 trees into one.
-    Note: all left tree's values must be less than all right tree's
+    合并左右子树
+    左子树所有节点小于右子树
     """
-    if (not left) or (not right):  # If one node is None, return the other
+    if (not left) or (not right):
         return left or right
     elif left.prior < right.prior:
-        """
-        Left will be root because it has more priority
-        Now we need to merge left's right son and right tree
-        """
+        "左子树作为根"
         left.right = merge(left.right, right)
         return left
     else:
-        """
-        Symmetric as well
-        """
         right.left = merge(left, right.left)
         return right
 
 
 def insert(root: Node, value: int) -> Node:
     """
-    Insert element
-    Split current tree with a value into left, right,
-    Insert new node into the middle
-    Merge left, node, right into root
+    插入元素
+    先划用当前值分左右子树
+    插入当前节点
+    合并各子树
     """
     node = Node(value)
     left, right = split(root, value)
@@ -95,10 +68,8 @@ def insert(root: Node, value: int) -> Node:
 
 def erase(root: Node, value: int) -> Node:
     """
-    Erase element
-    Split all nodes with values less into left,
-    Split all nodes with values greater into right.
-    Merge left, right
+    删除元素
+    先分后合
     """
     left, right = split(root, value - 1)
     _, right = split(right, value)
@@ -106,9 +77,7 @@ def erase(root: Node, value: int) -> Node:
 
 
 def inorder(root: Node):
-    """
-    Just recursive print of a tree
-    """
+    '中序遍历'
     if not root:  # None
         return
     else:
@@ -118,61 +87,26 @@ def inorder(root: Node):
 
 
 def interactTreap(root, args):
-    """
-    Commands:
-    + value to add value into treap
-    - value to erase all nodes with value
-        >>> root = interactTreap(None, "+1")
-        >>> inorder(root)
-        1 
-        >>> root = interactTreap(root, "+3 +5 +17 +19 +2 +16 +4 +0")
-        >>> inorder(root)
-        0 1 2 3 4 5 16 17 19 
-        >>> root = interactTreap(root, "+4 +4 +4")
-        >>> inorder(root)
-        0 1 2 3 4 4 4 4 5 16 17 19 
-        >>> root = interactTreap(root, "-0")
-        >>> inorder(root)
-        1 2 3 4 4 4 4 5 16 17 19 
-        >>> root = interactTreap(root, "-4")
-        >>> inorder(root)
-        1 2 3 5 16 17 19 
-        >>> root = interactTreap(root, "=0")
-        Unknown command
-    """
     for arg in args.split():
         if arg[0] == "+":
             root = insert(root, int(arg[1:]))
-
         elif arg[0] == "-":
             root = erase(root, int(arg[1:]))
-
         else:
-            print("Unknown command")
-
+            print("未知命令")
     return root
 
 
-def main():
-    """After each command, program prints treap"""
+if __name__ == "__main__":
     root = None
     print(
-        "enter numbers to create a tree, + value to add value into treap, "
-        "- value to erase all nodes with value. 'q' to quit. "
+        "输入数字建树"
+        "+1代表将1加入平衡树，-1代表把所有1删除"
+        "输入q退出"
     )
-
     args = input()
     while args != "q":
         root = interactTreap(root, args)
         print(root)
         args = input()
-
-    print("good by!")
-
-
-if __name__ == "__main__":
-    import doctest
-
-    doctest.testmod()
-    main()
-    
+    inorder(root)
